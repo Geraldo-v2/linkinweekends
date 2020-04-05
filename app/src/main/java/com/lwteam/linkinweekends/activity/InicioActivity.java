43bfe53f70f0009156c91d4dd50c8a59a5cfdc9f
Luidgi Samuel <luidgisamuel@users.noreply.github.com>
2020-04-04 23:32:09 -0300

    Update InicioActivity.java
    Adicionado os métodos para login com o Google

--- app/src/main/java/com/lwteam/linkinweekends/activity/InicioActivity.java
+++ app/src/main/java/com/lwteam/linkinweekends/activity/InicioActivity.java
@@ -30,8 +30,12 @@ import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
@@ -43,6 +47,7 @@ import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.database.core.Tag;
 import com.lwteam.linkinweekends.R;
 import com.lwteam.linkinweekends.helper.ConfiguracaoFirebase;
@@ -64,10 +69,10 @@ public class InicioActivity extends AppCompatActivity {
     private Usuario usuario;
     //IMPLEMENTACAO GOOGLE PRONTA ADICIONE AS LINHAS DE FUNCOES PARA O BOTAO
     GoogleSignInClient mGoogleSignInClient;
+    private static final int RC_SIGN_IN = 500;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         //BARRA DE NOTIFICAOES TRANSPARENTE
         requestWindowFeature(1);
         autenticacao = FirebaseAuth.getInstance();
@@ -83,6 +88,22 @@ public class InicioActivity extends AppCompatActivity {
 
         inicializarComponentes();
         verificarUsuarioLogado();
+        //Processo para autenticação com o Google
+        autenticacao=FirebaseAuth.getInstance();
+        SignInButton btGoogle = findViewById(R.id.btnContGoogle);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this,gso);
+
+        btGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
     }
 
     private void inicializarComponentes() {
@@ -176,11 +197,11 @@ public class InicioActivity extends AppCompatActivity {
     }
 
     //SEGUNDA PARTE DO FACEBOOK - ABRE A PAGINA PARA LOGIN NO FACEBOOK
-    @Override
+   /* @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         callbackManager.onActivityResult(requestCode,resultCode,data);
         super.onActivityResult(requestCode,resultCode,data);
-    }
+    }*/
 
     AccessTokenTracker tokenTracker = new AccessTokenTracker() {
         @Override
@@ -221,5 +242,53 @@ public class InicioActivity extends AppCompatActivity {
         }
     }
     //FIM DA SEGUNDA PARTE DO FACEBOOK
+
+    //INICIO DOS METODOS PARA LOGIN COM O GOOLE
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Resultado retornado do lançamento no Intent do GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In teve sucesso, autentica com  Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+
+            }
+        }else{
+            //Se o codigo requisitado não for RC_SIGN_IN então entra com o codigo do  facebook
+            callbackManager.onActivityResult(requestCode,resultCode,data);
+            super.onActivityResult(requestCode,resultCode,data);
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        autenticacao.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+                            Intent intent = new Intent(InicioActivity.this, MenuActivity.class);
+                            startActivity(intent);
+
+                        } else {
+                            Toast.makeText(InicioActivity.this, "Falha na autenticação", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+    //FIM DOS METÓDOS PARA LOGIN COM O GOOGLE
+
 }
 
